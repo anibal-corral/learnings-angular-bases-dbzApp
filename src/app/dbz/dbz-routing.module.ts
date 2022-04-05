880ec13ab9ef6bbf6477df4b30c362e2fbd84901
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-04-05 17:28:48 -0400

    Children Routes for Characters Home

--- src/app/dbz/dbz-routing.module.ts
+++ src/app/dbz/dbz-routing.module.ts
@@ -5,10 +5,12 @@ import { ListComponent } from './pages/list/list.component';
 import { AddComponent } from './pages/add/add.component';
 import { SearchComponent } from './pages/search/search.component';
 import { CharacterComponent } from './pages/character/character.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes:Routes=[
   {
-  path:'',
+  path:'home',
+  component:HomeComponent,
   children:[
 {path:'list', component:ListComponent},
 {path:'add', component:AddComponent},
